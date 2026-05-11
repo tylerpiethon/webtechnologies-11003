0aa054e461256e026c187d7bcfb05f8fcbea6e75
Morgan T. <metyler@ualr.edu>
2026-05-11 04:40:03 +0000

    changed site text color

---
+++ slick-demo/assets/js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+    $('.fade').slick({
+      dots: false,
+      infinite: true,
+      speed: 700,
+      fade: true,
+      slide: 'div',
+      cssEase: 'linear',
+      autoplay: true,
+      autoplaySpeed: 2000
+    });
+
+
+});
